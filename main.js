53bf4194fd0491ae48bae18cd7c534f420e0b4b1
isabellac2468 <99753386+isabellac2468@users.noreply.github.com>
2022-06-03 15:25:08 +0000

    latest

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+let dino = document.querySelector("#dino");
+let donkey = document.querySelector("#donkey")
+
+let game = document.querySelector("#game")
+
+game.addEventListener("mousemove", function (event) {
+  dino.style.left = `${event.offsetX}px`;
+  donkey.style.right = `${event.offsetX}px`;
+});
+
+let ball = document.querySelector("#ball")
+let y =0;
+let x = 0;
+function animateball() {
+y += 1;
+x += 1;
+  ball.style.top = `${y}px`
+  ball.style.left = `${x}px`
+  requestAnimationFrame(animateball);
+}
+
+animateball();
